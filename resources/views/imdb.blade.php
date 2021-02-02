e51934d6fe2b62685904d9e55679ab875f100031
Fijo-Lurk <filip.johansson@chasacademy.se>
2021-02-02 23:36:24 +0100

    adding more styling

--- resources/views/imdb.blade.php
+++ resources/views/imdb.blade.php
@@ -11,7 +11,7 @@
     <!-- component -->
 
 <main class="bg-yellow-200 grid place-items-center h-screen">
-    <h1 class="tracking-wide text-7xl font-mono">Hello World</h1>
+    <h1 class="tracking-wide text-5xl font-mono">Hello World</h1>
     <div class="relative">
       <div class="h-40 w-40 bg-pink-400 rounded-t-full relative overflow-hidden">
         <div class="h-40 w-40 bg-gradient-to-l from-pink-100 to-pink-400 rounded-t-full absolute left-12"></div>

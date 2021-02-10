92e1acf278f3e0c9a22dc92468f6f95124a812dd
thomm <thommie.wallin@chasacademy.se>
2021-02-10 18:10:42 +0100

    add seeder for genrestable and userstable, connected to migrationfiles

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('adminadmin')
+        ]);
     }
 }

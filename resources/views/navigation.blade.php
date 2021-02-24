dd99d88150720212f91e7f8f86b7d49ba3611603
Mehrdad <amini_pk@hotmail.com>
2021-02-24 20:24:06 +0100

    logo added

---
+++ resources/views/navigation.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>u05</title>
+</head>
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto flex items-center justify-between px-4 py-6+">
+            <ul class="flex item-center">
+                <li>
+                    <a href="#">
+                        <img class="w-28 mt-4 mb-2" src="../img/imdb_logo.svg">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 });
 
 Route::get('/', function () {
-    return view('imdb');
+    return view('navigation');
 });

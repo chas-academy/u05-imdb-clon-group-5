afe3d18b3fd14a44a1a04e46025950d4d8a65911
thommie-wallin <thommie.wallin@chasacademy.se>
2021-02-28 21:51:54 +0100

    created backpack:crud for user and movie, configured user

---
+++ app/Http/Controllers/Admin/MovieCrudController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\MovieRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class MovieCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class MovieCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\Movie::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/movie');
+        CRUD::setEntityNameStrings('movie', 'movies');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        // CRUD::setFromDb(); // columns
+
+        CRUD::column('title');
+        CRUD::column('director');
+        CRUD::column('writer');
+        CRUD::column('description');
+        CRUD::column('year');
+        CRUD::column('time');
+        CRUD::column('release_date');
+        CRUD::column('img-url')->type('image');
+
+        /**
+         * Columns can be defined using the fluent syntax or array syntax:
+         * - CRUD::column('price')->type('number');
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(MovieRequest::class);
+
+        // CRUD::setFromDb(); // fields
+
+        CRUD::field('title');
+        CRUD::field('director');
+        CRUD::field('writer');
+        CRUD::field('description')->type('textarea');
+        CRUD::field('year')->type('number');
+        CRUD::field('time')->type('time');
+        CRUD::field('release_date')->type('date');
+        CRUD::field('img-url')->type('image');
+
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - CRUD::field('price')->type('number');
+         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,4 +14,6 @@ Route::group([
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::crud('user', 'UserCrudController');
+    Route::crud('movie', 'MovieCrudController');
 }); // this should be the absolute last line of this file

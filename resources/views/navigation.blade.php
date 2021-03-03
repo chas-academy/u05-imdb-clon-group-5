771a39cc0305dd4dbee81400ffa7a3eae5f1d971
Mehrdad <amini_pk@hotmail.com>
2021-03-03 09:05:05 +0100

    magnifier added & fixed styling

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -11,7 +11,7 @@
     <div class="container mx-auto border-b bg-gray-800 border-gray-800 p-5">
     <header class="flex flex-row justify-between">
         <a href="#" class="x1 mt-2">
-            <img class="md:w-12 lg:w-24" src="../img/imdb_logo.svg" alt="Imdb Logo" title="Imdb Logo">
+            <img class="md:w-12 lg:w-24 lg:mt-3" src="../img/imdb_logo.svg" alt="Imdb Logo" title="Imdb Logo">
         </a>
 
         <!-- search here! -->
@@ -25,25 +25,24 @@
         </a>
     </div> -->
 
-    <div class="x1 mt-2 lg:ml-auto w-3/6">
-    <div class="hidden sm:hidden md:flex lg:w-26 lg:mt-1.5 max-w-screen-lg bg-blue-200 items-center rounded-full shadow-xl h-5 p-4">
-    <input class="rounded-l-full w-full py0 px0 text-gray-700 leading-tight bg-blue-200 focus:outline-none h-2 p-2" id="search" type="text" placeholder="Search">        
-
-    <div class="x3 ml-auto">
-      <button class="lg:w-24 top-1/2 left-1/2  bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-5 flex items-center justify-center">
-        icon
-      </button>
-      </div>
+  <div class="x1 mt-3.5 lg:ml-auto w-3/6">
+    <div class="hidden sm:hidden md:flex lg:w-26 lg:mt-1.5 max-w-screen-lg bg-blue-100 items-center rounded-full shadow-xl lg:h-30 p-2">
+        <div class="x4 mt-2">
+            <img class="w-9 mb-1 mr-auto " src="../img/magnifier.svg" alt="magnifier" title="magnifier">
+        </div>
+    <input class="rounded-l-full w-full py0 px0 text-gray-700 leading-tight bg-blue-100 focus:outline-none h-2 p-2" id="search" type="text" placeholder="Search">        
+        <div class="x3 ml-auto h-7">
+            <button class="lg:w-24 top-1/2 left-1/2 bg-white text-blue-600 rounded-full p-2 hover:bg-gray-300 focus:outline-none w-12 h-7 flex items-center justify-center">
+                GO
+            </button>
+        </div>
     </div>
   </div>
-
-
-
         <a href="#" class="x2 ml-auto mt-3">
-            <img class="md:w-16 md:mt-2 lg:w-20" src="../img/login_logo.svg" alt="Login Logo" title="Login Logo">
+            <img class="md:w-16 md:mt-3.5 lg:w-20" src="../img/login_logo.svg" alt="Login Logo" title="Login Logo">
         </a>
-        <p id="burgerbtn" class="lg:w-24 lg:ml-5 ml-3 mt-3 md:hidden">
-            <img class="md:w-8 md:ml-3 lg:w-24" src="../img/navigation_logo.svg" alt="Navigation Logo" title="Navigation Logo">
+        <p id="burgerbtn" class="lg:w-24 lg:ml-5 ml-3 mt-3.5 md:hidden">
+            <img class="md:w-8 md:ml-3 mt-1 lg:w-24" src="../img/navigation_logo.svg" alt="Navigation Logo" title="Navigation Logo">
         </p>
 </header>
 <nav>

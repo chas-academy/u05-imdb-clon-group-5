b1a350ee16726c19af214cdc467a9564c40952f6
Natalie Nilsson <natalie.nilsson93.nn@gmail.com>
2021-03-04 11:16:20 +0100

    added picture

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,24 @@
+@extends('layout/layout')
+@section('footer')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" class="poppins">
+    <title>Footer</title>
+</head>
+<body>
+    
+    <footer>
+
+    <img id="imdb" src="imdb.png" alt="imdb">
+    <img id ="cat" src="cat.png" alt="cat">
+    <a id="about" href="#">About us</a>
+
+    </footer> 
+
+</body>
+</html>

c67022a3bf479a59451cadad7ef26f152de841da
Natalie Nilsson <natalie.nilsson93.nn@gmail.com>
2021-03-04 14:05:53 +0100

    added genre text

---
+++ resources/views/genre.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Poppins:ital,wght@0,400;0,900;1,400;1,500;1,600;1,700;1,800&display=swap">
+    <link rel="stylesheet" href="genre.css">
+    <title>Genre</title>
+</head>
+<body>
+    <ul class="genre">
+        <li>
+            <a href="#">Action</a> 
+        </li>
+
+        <li>
+            <a href="#">Animation</a> 
+        </li>
+
+        <li>
+            <a href="#">Comedy</a> 
+        </li>
+
+        <li>
+            <a href="#">Crime</a> 
+        </li>
+
+        <li>
+            <a href="#">Drama</a> 
+        </li>
+
+        <li>
+            <a href="#">Fantasy</a> 
+        </li>
+    
+        <li>
+            <a href="#">Horror</a> 
+        </li>
+
+        <li>
+            <a href="#">Romance</a> 
+        </li>
+
+        <li>
+            <a href="#">Science fiction</a> 
+        </li>
+
+        <li>
+            <a href="#">Thriller</a> 
+        </li>
+  
+</ul>
+
+</body>
+</html>

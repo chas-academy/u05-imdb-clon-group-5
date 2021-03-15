a17cb14428aad00fd6f137ddcd97c602e400aa15
Natalie Nilsson <natalie.nilsson93.nn@gmail.com>
2021-03-15 10:58:41 +0100

    added picture

--- resources/views/choosegenre.blade.php
+++ resources/views/choosegenre.blade.php
@@ -1,17 +1,19 @@
 @extends('layout/layout')
 @section('choosegenre')
 
-<div class="bg-midnight-500 text-white text-center">
 
-    
 
-    <p class="inline text-white">Choose a genre that suits your palate</p>
+<div class="bg-midnight-500 h-screen py-20">
 
-    <p class="inline text-white">Search among your favorites</p>
+    <div class="font-poppins text-white text-center">
 
-    <button class="inline text-white">Choose your genre</button> 
+        <p class="text-5xl sm:text-6xl">Choose a genre that suits your palate</p>
 
-    
+        <p class="text-4xl sm:text-5xl">Search among your favorites</p>
+
+        <button class="text-2xl sm:text-4xl bg-#C34A36">Choose your genre<img class="inline object-contain md:object-scale-down" src="../img/choosegenreplay.png" alt="playbutton"></button> 
+
+    </div> 
 
 </div>
 

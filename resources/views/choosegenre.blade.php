af31f6c9094309340cfd38c5a0e835fba3e09c10
Natalie Nilsson <natalie.nilsson93.nn@gmail.com>
2021-03-15 11:24:04 +0100

    done

--- resources/views/choosegenre.blade.php
+++ resources/views/choosegenre.blade.php
@@ -11,8 +11,7 @@
 
         <p class="text-4xl sm:text-5xl">Search among your favorites</p>
 
-        <button class="text-2xl sm:text-4xl bg-#C34A36 px-4 rounded-full">Choose your genre<img class="inline object-contain md:object-scale-down" src="../img/choosegenreplay.png" alt="playbutton"></button> 
-
+        <a href="/genre.blade.php" class="text-2xl sm:text-4xl bg-mojo-500 px-4 rounded-full btn btn-primary btn-lg active" role="button">Choose your genre<img class="inline object-contain md:object-scale-down" src="../img/choosegenreplay.png" alt="playbutton"></a>
     </div> 
 
 </div>

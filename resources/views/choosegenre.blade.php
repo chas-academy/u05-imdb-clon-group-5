11ad0e2078a85b5c069d94f6496b432ee56007c0
Natalie Nilsson <natalie.nilsson93.nn@gmail.com>
2021-03-15 18:14:05 +0100

    done

--- resources/views/choosegenre.blade.php
+++ resources/views/choosegenre.blade.php
@@ -1,19 +1,22 @@
 @extends('layout/layout')
-@section('choosegenre')
+@section('content')
 
 
 
-<div class="bg-midnight-500 h-screen py-20">
+<div class="bg-midnight-500 h-screen pt-40">
 
     <div class="font-poppins text-white text-center">
 
-        <p class="text-5xl sm:text-6xl">Choose a genre that suits your palate</p>
+        <p class="m-6 text-3xl sm:text-4xl">Choose a genre that suits your palate</p>
 
-        <p class="text-4xl sm:text-5xl">Search among your favorites</p>
+        <p class="m-6 text-2xl sm:text-3xl">Search among your favorites</p>
 
-        <a href="/genre.blade.php" class="text-2xl sm:text-4xl bg-mojo-500 px-4 rounded-full btn btn-primary btn-lg active" role="button">Choose your genre<img class="inline object-contain md:object-scale-down" src="../img/choosegenreplay.png" alt="playbutton"></a>
-    </div> 
-
-</div>
+     
+            <a href="/genre.blade.php"> 
+            <button class="font-poppins text-center bg-mojo-500 hover:bg-mojo-400 text-2xl sm:text-1xl text-white py-2 px-3 pl-10 rounded-full">Choose your genre
+            <img class="mr-6 ml-1 inline object-contain md:object-scale-down" src="../img/choosegenreplay.png" alt="playbutton"></button></a>
+    
+    </div>
+</div> 
 
 @endsection

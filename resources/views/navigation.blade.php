ac5cb5affa3c607e00b12782ea7757385b73d029
Mehrdad <amini_pk@hotmail.com>
2021-03-16 21:52:12 +0100

    responsive styling

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -44,7 +44,7 @@
                 <div class="x1 lg:auto w-3/6 md:mt-1" style="margin-left: auto;margin-right: auto;" class="md:flex md:justify-center md:items-center" id="mobs" id="burgerbtn">
                     <form class="hidden sm:hidden md:flex lg:w-26 lg:mt-1.5 max-w-screen-lg bg-gradient-to-l from-blue-300 to-blue-500 items-center rounded-full shadow-xl md:h-9 p-2 md:mt-1">
                         <img class="w-9 mb-1 mr-auto mt-1" src="./img/magnifier.svg" alt="magnifier" title="magnifier">
-                        <input class="rounded-l-full w-full py0 px0 bg-gradient-to-l from-blue-300 to-blue-500 text-gray-700 leading-tight focus:outline-none h-2 p-2" id="search" type="text" placeholder="Search">
+                        <input class="rounded-l-full w-full py0 px0 bg-gradient-to-l from-blue-300 to-blue-500 text-white leading-tight focus:outline-none h-2 p-2" id="search" type="text" placeholder="Search">
                         <button class="lg:w-24 top-1/2 left-1/2 bg-white text-blue-600 rounded-full p-2 hover:bg-gray-300 focus:outline-none w-16 h-6 flex items-center justify-center">GO</button>
                     </form>
                 </div>
@@ -89,7 +89,7 @@
                         <div class="lg:mt-24 md:10">
 
                     <div class="containerx mt-7">
-                          <a class="imagex p-2 no-underline text-4xl lg:text-6xl text-white block duration-75 hover:bg-gray-800 focus:ring-gray-200top-1 right-2 lg:mb-2 md:mb-1 sm:mb-1" href="#">Movies
+                          <a class="imagex p-2 no-underline text-4xl lg:text-6xl text-white block duration-75 hover:bg-gray-800 focus:ring-gray-200top-1 right-2 lg:mb-2 md:mb-1 sm:mb-1 xl:text-8xl" href="#">Movies
                             <div class="hidden sm:hidden lg:flex overlayx">
                               <div class="hidden sm:hidden lg:flex textx">
                                 <img class="" src="./img/movies-hover-frame.svg" alt="movies-hover-frame" title="movies-hover-frame">
@@ -99,7 +99,7 @@
                         </div>
 
                         <div class="containerx mt-7">
-                          <a class="imagex p-2 no-underline text-4xl lg:text-6xl text-white block duration-75 hover:bg-gray-800 focus:ring-gray-200 top-1 right-2 lg:mb-2 md:mb-1" href="#">Tv shows
+                          <a class="imagex p-2 no-underline text-4xl lg:text-6xl text-white block duration-75 hover:bg-gray-800 focus:ring-gray-200 top-1 right-2 lg:mb-2 md:mb-1 xl:text-8xl" href="#">Tv shows
                             <div class="hidden sm:hidden lg:flex overlayx">
                               <div class="hidden sm:hidden lg:flex  textx">
                                 <img class="" src="./img/tv-shows-hover-frame.svg" alt="tv-shows-hover-frame" title="tv-shows-hover-frame">
@@ -109,7 +109,7 @@
                         </div>
 
                         <div class="containerx mt-7">
-                          <a class="imagex p-2 no-underline text-4xl lg:text-6xl text-white block duration-75 hover:bg-gray-800 focus:ring-gray-200 top-1 right-2 lg:mb-2 md:mb-1" href="#">Genre
+                          <a class="imagex p-2 no-underline text-4xl lg:text-6xl text-white block duration-75 hover:bg-gray-800 focus:ring-gray-200 top-1 right-2 lg:mb-2 md:mb-1 xl:text-8xl" href="#">Genre
                             <div class="hidden sm:hidden lg:flex overlayx">
                               <div class="hidden sm:hidden lg:flex textx">
                                 <img class="" src="./img/genre-hover-frame.svg" alt="genre-hover-frame" title="genre-hover-frame">
@@ -119,7 +119,7 @@
                         </div>
 
                         <div class="containerx mt-7">
-                          <a class="imagex p-2 no-underline text-4xl lg:text-6xl text-white block duration-75 hover:bg-gray-800 focus:ring-gray-200 top-1 right-2 lg:mb-2 md:mb-1" href="#">Watchlist
+                          <a class="imagex p-2 no-underline text-4xl lg:text-6xl text-white block duration-75 hover:bg-gray-800 focus:ring-gray-200 top-1 right-2 lg:mb-2 md:mb-1 xl:text-8xl" href="#">Watchlist
                             <div class="hidden sm:hidden lg:flex overlayx l">
                               <div class="hidden sm:hidden lg:flex textx">
                                 <img class="" src="./img/watchlist-hover-frame.svg" alt="watchlist-hover-frame" title="watchlist-hover-frame">
@@ -186,18 +186,18 @@
                     <div class="p-2 w-full justify-center inline-flex ml-3" style="margin-top: 3rem;padding-left: 2rem;left: 50%;margin-left: -0.75rem;">
                         <!-- added style -->
 
-                        <div class="rounded-l-full bg-white flex items-center rounded-full shadow-xl h-8">                            
-                            <span class="rounded-l-full w-12 h-8 flex justify-end items-center text-gray-500 p-2 bg-blue-300 p-1.5 md:left0" style="padding: 0rem;">
-                                <img class="w-20 mb-1 mr-auto mt-1 bg-blue-300 h-8 " style="position: relative;left: 0.75rem;width: 2rem;" src=" ./img/magnifier.svg" alt="magnifier" title="magnifier"> 
+                        <div class="rounded-l-full bg-white flex items-center rounded-full shadow-xl h-8 xl:h-16">                            
+                            <span class="rounded-l-full w-12 h-8 lg:h-10 flex justify-end items-center text-gray-00 p-2 bg-blue-500 p-1.5 md:left0  xl:h-16" style="padding: 0rem;">
+                                <img class="w-20 mb-1 mr-auto mt-1 bg-blue-500 h-8 " style="position: relative;left: 0.75rem;width: 2rem;" src=" ./img/magnifier.svg" alt="magnifier" title="magnifier"> 
                             </span>
 
-                            <input class="w-full py-4 px-6 bg-gradient-to-l from-blue-500 to-blue-300 text-gray-200 leading-tight focus:outline-none h-8 text-xl" id="search" type="text" placeholder="Search">
+                            <input class="w-full py-4 px-6 bg-gradient-to-l from-blue-300 to-blue-500 text-white leading-tight focus:outline-none h-8 lg:h-10 text-xl xl:h-16" id="search" type="text" placeholder="Search">
                             <!-- <form class="hidden sm:hidden md:flex lg:w-26 lg:mt-1.5 max-w-screen-lg bg-gradient-to-l from-blue-300 to-blue-500 items-center rounded-full shadow-xl md:h-8 p-2">
                             <img class="w-9 mb-1 mr-auto mt-1" src="./img/magnifier.svg" alt="magnifier" title="magnifier"> -->
 
 
-                            <div class="p-1 h-8  rounded-r-full bg-blue-500">
-                                <button class="lg:w-24 top-1/2 left-1/2 bg-white text-indigo-700 rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-6 flex items-center justify-center text-xl">
+                            <div class="p-1 h-8 lg:h-10 rounded-r-full bg-blue-300 xl:h-16">
+                                <button class="lg:w-24 top-1/2 left-1/2 bg-white text-indigo-500 rounded-full p-2 hover:bg-blue-300 focus:outline-none w-12 h-6 lg:h-8 flex items-center justify-center text-xl xl:h-14">
                                     Go
                                 </button>
                                 </form>

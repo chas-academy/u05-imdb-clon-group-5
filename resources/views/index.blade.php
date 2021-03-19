97184c1366ed617760248035bd794d54f3f6a6c7
Fijo-Lurk <filip.johansson@chasacademy.se>
2021-03-19 21:32:53 +0100

    removing old home styling. adding new html and started to add tailwindcss and added two img for hero section on home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,54 @@
 @extends('layout/layout')
 @section('content')
   {{--   Heropage --}}
-<div class="relative m-auto bg-midnight">
-    <img class="w-full h-full" src="../img/hero-page-bg.jpg">
-
-
-        <img class="w-11/12 h-11/12 inset-center" src="../img/hero-movie-bg.svg">
-      
-        <div class="w-11/12 h-11/12 inset-center">
-
-            <div class="ml-2 md:ml-4 lg:ml-8 h-full mb-20  sm:mb-36" id="home-text-containers">
-                <h1 class="font-black tracking-wider uppercase text-white text-2xl sm:font-inter sm:text-5xl lg:text-6xl xl:text-8xl">
-                    extraction</h1>
-                <h3 class="uppercase font-semibold mt-1 text-white text-sm sm:text-2xl lg:text-3xl xl:text-4xl">official trailer</h3>
-                <h4 class="uppercase font-semibold text-white text-xs sm:text-2xl lg:text-3xl xl:text-4xl">3:02</h4>
-               <div class="lg:mt-6 sm:mt-3">
-                <a href="https://www.youtube.com/watch?v=L6P3nI6VnlY&t=1s" class="px-2 py-1 mt-3 font-normal tracking-wide text-xs
-                text-white capitalize transition-colors duration-200
-                transform bg-blue-600 rounded-md dark:bg-gray-800
-               hover:bg-blue-500 dark:hover:bg-gray-700 focus:outline-none
-                focus:bg-blue-500 dark:focus:bg-gray-700 sm:px-6 sm:py-2 lg:px-12 lg:py-2 xl:px-14 xl:py-3 xl:text-2xl">
-               PLAY
-           </a>
-               </div>
-
+  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div class="flex flex-col items-center justify-between w-full mb-10 lg:flex-row">
+      <div class="mb-16 lg:mb-0 lg:max-w-lg lg:pr-5">
+        <div class="max-w-xl mb-6">
+          <div>
+            <p class="inline-block text-red-600 py-px mb-4 text-xs font-semibold tracking-wider uppercase rounded-full">
+              Brand new
+            </p>
+          </div>
+          <h2 class="max-w-lg mb-6 font-poppins text-3xl text-gray-200 font-bold tracking-tight sm:text-4xl sm:leading-none ">
+            Everything you<br class="hidden md:block" />
+            can imagine
+            <span class="inline-block text-red-600">is real</span>
+          </h2>
+          <p class="text-base text-gray-200 md:text-lg">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.
+          </p>
         </div>
+       
+      </div>
+      <div class="flex items-center justify-center lg:w-1/2">
+        <div class="w-2/5">
+          <img class="object-cover" src="../img/iphone-12-silver-prisoner.png" alt="Prisoners movie poster" />
+        </div>
+        <div class="w-5/12 -ml-16 lg:-ml-32">
+          <img class="object-cover" src="../img/iphone-12-silver-birds-of-prey.png" alt="birds of prey movie poster" />
+        </div>
+      </div>
     </div>
-    
-    
-</div> 
+    <a
+      href="#scroll-down"
+      aria-label="Scroll down"
+      class="flex items-center justify-center w-10 h-10 mx-auto text-gray-600 duration-300 transform border border-gray-400 rounded-full hover:text-deep-purple-accent-400 hover:border-deep-purple-accent-400 hover:shadow hover:scale-110"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="currentColor">
+        <path d="M10.293,3.293,6,7.586,1.707,3.293A1,1,0,0,0,.293,4.707l5,5a1,1,0,0,0,1.414,0l5-5a1,1,0,1,0-1.414-1.414Z"></path>
+      </svg>
+    </a>
+  </div>
+
+
 {{-- carousel-featured-now --}}
+
+
+
 <div class="ml-2">
 <h1 class="font-bold text-4xl font-inter text-red-500  sm:text-5xl">Featured</h1>
-<h1 class="text-white text-4xl font-inter sm:text-5xl">
+<h1 id="scroll-down" class="text-white text-4xl font-inter sm:text-5xl">
     <span class="font-bold">Now</span>
      showing</h1>
     </div>

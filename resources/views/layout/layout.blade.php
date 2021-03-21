8a4054a95f3e0cccae69c152a760e352cca54bc3
Fijo-Lurk <filip.johansson@chasacademy.se>
2021-03-21 14:03:08 +0100

    adding glider.js

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,6 +7,8 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script defer src="{{ asset('js/code.js')}}"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.core.min.css">
+    <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.theme.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <title>Movie-db</title>
 </head>

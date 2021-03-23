073be5e237b2e2dd999481c28025e3e624aa6983
Mehrdad <amini_pk@hotmail.com>
2021-03-23 16:09:29 +0100

    SearchController created

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+        $posts = Post::query()
+            ->where('title', 'LIKE', "%{$search}%")
+            ->orWhere('body', 'LIKE', "%{$search}%")
+            ->get();
+        return view('search', compact('posts'));
+    }
+}

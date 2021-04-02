27985f3754ece361b5cb799ba001d86ee5c096b9
Fijo-Lurk <filip.johansson@chasacademy.se>
2021-04-02 12:16:51 +0200

    rework-

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -150,22 +150,13 @@
 
         <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
 
-            <div
-                class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
-                <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                    viewBox="0 0 18 18">
-                    <path
-                        d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
-                    </path>
-                </svg>
-                <span class="text-sm">(Esc)</span>
-            </div>
 
-            <!-- Add margin if you want to see some of the overlay behind the modal-->
+
+
             <div class="modal-content py-4 text-left px-6">
                 <!--Title-->
                 <div class="flex justify-between items-center pb-3">
-                    <p class="text-2xl font-bold">Movie Review</p>
+                    <p class="text-2xl font-bold">Movie Review 🎥</p>
                     <div class="modal-close cursor-pointer z-50">
                         <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
                             viewBox="0 0 18 18">
@@ -213,28 +204,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <section id="writeReview" class="hidden  max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-
-    </section>
 
 
 

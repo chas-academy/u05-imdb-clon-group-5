383a382153dd22e6c4703927f3490d650b525f04
thommie-wallin <thommie.wallin@chasacademy.se>
2021-04-04 15:46:36 +0200

    changed name FooterComponent.php and class name to the same

--- app/View/Components/FooterComponent.php
+++ app/View/Components/FooterComponent.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class footer_component extends Component
+class FooterComponent extends Component
 {
     /**
      * Create a new component instance.

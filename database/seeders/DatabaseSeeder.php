3365c9aa8ba6e27690da0ac34d35d6f9d8054594
thommie-wallin <thommie.wallin@chasacademy.se>
2021-04-05 16:33:38 +0200

    calling seeders in databaseseeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
+        $this->call([
+            UsersTableSeeder::class,
+            MoviesTableSeeder::class,
+            GenresTableSeeder::class,
+            GenreItemTabelSeeder::class,
+        ]);
     }
 }

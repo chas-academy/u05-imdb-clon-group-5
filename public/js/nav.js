38140edea12b4edb20d97d4c3e5a66855e54106b
Fijo-Lurk <filip.johansson@chasacademy.se>
2021-04-06 18:10:14 +0200

    removed this js code becuse we don't need it just cause error in console. we are using auth to show login or logout

--- public/js/nav.js
+++ public/js/nav.js
@@ -5,9 +5,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
-
-const logout = document.querySelector("#logout");
-const logoutbtn = document.querySelector("#logoutbtn");
-logoutbtn.addEventListener("click", () => {
-    logout.classList.remove("hidden");
-});

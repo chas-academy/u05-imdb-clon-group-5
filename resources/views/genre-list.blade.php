0881508a65e1203d8057042e52d0d50e88978c31
Fijo-Lurk <filip.johansson@chasacademy.se>
2021-04-06 17:41:57 +0200

    fix genre

--- resources/views/genre-list.blade.php
+++ resources/views/genre-list.blade.php
@@ -3,7 +3,7 @@
 
 <div class="bg-midnight-500 py-10">
 
-    <ul class="font-Inter text-3xl lg:text-4xl text-white text-center 2xl:text-6xl">
+    <ul class="font-Inter text-3xl lg:text-4xl text-white text-center 2xl:text-5xl">
        @foreach ($genres as $genre)
            <li class="mt-2"><a class="hover:text-red-700" href="/genre/{{$genre->id}}">{{$genre->genre}}</a></li>
 
